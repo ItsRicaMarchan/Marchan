9a3e13a29d523614e5af05c90e4f86e590fad406
ItsRicaMarchan <marmolmarchanj@gmail.com>
2023-03-14 17:09:07 +0800

    POST NEW-RICA

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { LoginComponent } from './login/login.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
+import {PostComponent} from './post/post.component';
+
 
 
 
 const routes: Routes = [
   {path: 'signup' , component: SignUpComponent},
+  {path: 'post', component: PostComponent},
   {path: 'loger' , component: LoginComponent},
   {path: '', redirectTo: 'register', pathMatch: 'full'},
-  {path: 'dashboard' , component: DashboardComponent}
+  {path: 'dashboard' , component: DashboardComponent},
+ 
  
   
  
@@ -23,3 +27,7 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const root = [
+  SignUpComponent,LoginComponent,DashboardComponent
+
+]
